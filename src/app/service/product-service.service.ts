a6f2e3865e037aa38030e59045baec1a9a9df554
Diogo Emannuel <diogoemannuel.rodrigues1@gmail.com>
2021-05-25 10:48:29 -0300

    ajuste

--- src/app/service/product-service.service.ts
+++ src/app/service/product-service.service.ts
@@ -11,13 +11,6 @@ export class ProductServiceService {
   constructor(private http: HttpClient) { }
   API  = 'http://localhost:8080/produtos/';
 
- /*  public createProdutos(productCrud: ProductCrud): Observable<ProductCrud[]> {
-    return this.http.get<ProductCrud[]>('http://localhost:8080/produtos');
-  } */
-
- /*  public create(produto: Produto): Observable<Produto>{
-    return this.http.post<Produto>(this.API, produto)
-  } */
   public create(produto: Produto) {
     console.log('Id Produto ' + produto.id);
     return this.http.post('http://localhost:8080/produtos', produto);
