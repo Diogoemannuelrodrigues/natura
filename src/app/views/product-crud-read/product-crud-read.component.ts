2a5ebbb5098e1886c8d0c6fdb940d67c141196d6
Diogo Emannuel <diogoemannuel.rodrigues1@gmail.com>
2021-05-28 09:05:38 -0300

    datasource funcionando - OK

--- src/app/views/product-crud-read/product-crud-read.component.ts
+++ src/app/views/product-crud-read/product-crud-read.component.ts
@@ -10,16 +10,8 @@ import { MatTableDataSource } from '@angular/material/table';
 })
 export class ProductCrudReadComponent implements OnInit {
 
-   produtos: Produto[] = [
-    {id_produto: 1, nome: 'Hydrogen', preco: '1.0', codigoProduto: '1'},
-    {id_produto: 2, nome: 'Helium', preco: '4.00', codigoProduto: '2'},
-    {id_produto: 3, nome: 'Lithium', preco: '6.94', codigoProduto: '3'},
-    {id_produto: 4, nome: 'Beryllium', preco: '9.01', codigoProduto: '2'},
-    {id_produto: 5, nome: 'Boron', preco: '10.81', codigoProduto: '1'},
-  ];
-  displayedColumns: string[] = ['id_produto', 'nome', 'preco', 'codigoProduto', 'descricao', ];
-  newprodutos: Produto[] = new Array;
-  dataSource = new MatTableDataSource(this.produtos);
+  produtos: Produto[] = [];
+  displayedColumns = ['id_produto', 'nome', 'preco', 'codigoProduto', 'descricao' ];
 
   constructor(private service: ProductServiceService) { }
 
@@ -29,14 +21,10 @@ export class ProductCrudReadComponent implements OnInit {
 
   getProdutos(){
     this.service.getProdutos().subscribe(produtos => {
-      this.newprodutos = produtos;
+      this.produtos = produtos;
       console.log(produtos);
     })
   }
 
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
 
 }
